d0ded668b8e908dd03891e70b8dc87f665b1ce8e
Bernold Rodrigo Abarca Zuniga <bernold.rodrigo.abarca.zuniga@univie.ac.at>
2026-04-17 14:11:14 +0200

    Remove __pycache__ and compiled python files

--- baselines/SuffixTransformerNetwork/LSTM_seq2seq/__init__.py
+++
@@ -1,3 +0,0 @@
-"""Package containing all functionality needed for training and evaluating 
-the LSTM encoder-decoder benchmark. 
-"""
--- baselines/SuffixTransformerNetwork/OneStepAheadBenchmarks/__init__.py
+++
@@ -1,2 +0,0 @@
-"""Contains all the modules needed for converting the data for the 
-SEP benchmark, training them, and evaluating them."""
